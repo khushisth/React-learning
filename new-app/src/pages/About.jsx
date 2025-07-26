33e6ccb9925bdad13fa065f877ff50de86a04b5e
khushisth <khushishresthahappy@gmail.com>
2025-07-26 22:56:30 +0545

    added another function

--- new-app/src/pages/About.jsx
+++ new-app/src/pages/About.jsx
@@ -18,6 +18,7 @@ function About(){
     <button onClick={()=> setValue (value + 1)} className="px-6 py-3 bg-blue-600 text-white font-medium rounded-lg shadow-lg hover:bg-blue-700 transition w-fit">Click me(+)</button>
     <button onClick={()=> setValue (value - 1)} className="px-6 py-3 bg-blue-600 text-white font-medium rounded-lg shadow-lg hover:bg-blue-700 transition w-fit">Click me(-)</button>
     <button onClick={()=> setValue (value * 2)} className="px-6 py-3 bg-blue-600 text-white font-medium rounded-lg shadow-lg hover:bg-blue-700 transition w-fit">Click me(*)</button>
+    <button onClick={()=> setValue (value /5)} className="px-6 py-3 bg-blue-600 text-white font-medium rounded-lg shadow-lg hover:bg-blue-700 transition w-fit">Click me(+)</button>
     </div>
     
     <span className="text-black text-bold">{online?"User is online":"connecting..."}</span>
