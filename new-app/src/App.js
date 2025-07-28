496ffbb251b55c18ccc5a00b15522b7a96a69fe5
khushisth <khushishresthahappy@gmail.com>
2025-07-28 22:57:36 +0545

    user login

--- new-app/src/App.js
+++ new-app/src/App.js
@@ -1,13 +1,14 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import Home from './pages/Homes'
 import About from './pages/About';
+import Login from './pages/Login';
 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login/>} />
         <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
---
+++ new-app/src/pages/Login.jsx
@@ -0,0 +1,59 @@
+
+import { useState } from "react";
+export default function Login() {
+    //email
+    const[email,setEmail]=useState("");
+    //password
+    const[password,setPassword]=useState("");
+    function handleSubmit(e){
+        e.preventDefault();
+        const loginData={
+            email,
+            password,
+        }
+        const tokenValue ="12345";
+        localStorage.setItem("token",tokenValue)
+        console.log(loginData);
+    }
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="w-full max-w-sm bg-white rounded-md shadow-md p-8">
+        <div className="text-center text-xl font-medium text-gray-800 mb-6">
+          👤
+        </div>
+
+        <div className="text-center text-xl font-medium text-gray-800 mb-6">
+          HRMS Login
+        </div>
+
+        <div className="mb-4">
+          <span className="block text-sm text-gray-700 mb-1">Email</span>
+          <input
+            type="email"
+            placeholder="admin@example.com"
+            value={email}
+            onChange={(e)=> setEmail(e.target.value)}
+            className="w-full border border-gray-300 rounded-md px-3 py-2 text-sm text-gray-900 "
+          />
+        </div>
+
+        <div className="mb-6">
+          <span className="block text-sm text-gray-700 mb-1">Password</span>
+          <input
+            type="password"
+            placeholder="••••••••"
+            value={password}
+            onChange={(e)=> setPassword(e.target.value)}
+            className="w-full border border-gray-300 rounded-md px-3 py-2 text-sm text-gray-900 "
+          />
+        </div>
+
+        <div>
+          <button onClick={handleSubmit} className="block text-center  cursor-pointer  bg-black text-white py-7 px-10 rounded-md ">
+            Login
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
